b7db2cd92dc322c540d9ff3212c1fa8e4ae0d864
Chiu, Yung-Hsiang <mirror.tw@gmail.com>
2021-10-29 11:33:27 +0800

    fix: upgrade to gas-client 1.0.0

--- src/client/utils/server.js
+++ src/client/utils/server.js
@@ -1,8 +1,8 @@
-import Server from 'gas-client';
+import { GASClient } from 'gas-client';
 
 const { PORT } = process.env;
 
-const server = new Server({
+const server = new GASClient({
   // this is necessary for local development but will be ignored in production
   allowedDevelopmentDomains: `https://localhost:${PORT}`,
 });
