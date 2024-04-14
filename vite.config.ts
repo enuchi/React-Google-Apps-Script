58c6e5fc82f5b9c09500cc6fe55e1cfdb785003b
elisha <e.nuchi@gmail.com>
2024-04-14 13:00:29 -0400

    re-factor vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
-import { build, defineConfig } from 'vite';
-import { readFileSync } from 'fs';
+import { BuildOptions, ServerOptions, build, defineConfig } from 'vite';
+import { existsSync, readFileSync } from 'fs';
 import react from '@vitejs/plugin-react-swc';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { viteSingleFile } from 'vite-plugin-singlefile';
@@ -9,6 +9,9 @@ import { writeFile } from 'fs/promises';
 const PORT = 3000;
 const clientRoot = './src/client';
 const outDir = './dist';
+const serverEntry = 'src/server/index.ts';
+const copyAppscriptEntry = './appsscript.json';
+const devServerWrapper = './dev/dev-server-wrapper.html';
 
 const clientEntrypoints = [
   {
@@ -43,20 +46,46 @@ const clientEntrypoints = [
   },
 ];
 
+const keyPath = resolve(__dirname, './certs/key.pem');
+const certPath = resolve(__dirname, './certs/cert.pem');
+const pfxPath = resolve(__dirname, './certs/cert.pfx'); // if needed for Windows
+
+const devServerOptions: ServerOptions = {
+  port: PORT,
+};
+
+// use key and cert settings only if they are found
+if (existsSync(keyPath) && existsSync(certPath)) {
+  devServerOptions.https = {
+    key: readFileSync(resolve(__dirname, './certs/key.pem')),
+    cert: readFileSync(resolve(__dirname, './certs/cert.pem')),
+  };
+}
+
+// If mkcert -install cannot be used on Windows machines (in pipeline, for example), the
+// script at test/generate-cert.ps1 can be used to create a .pfx cert
+// if (existsSync(pfxPath)) {
+//   // use pfx file if it's found
+//   devServerOptions.https = {
+//     pfx: readFileSync(pfxPath),
+//     passphrase: 'abc123',
+//   };
+// }
+
 const clientServeConfig = () =>
   defineConfig({
     plugins: [react()],
-    server: {
-      port: PORT,
-      https: {
-        key: readFileSync(resolve(__dirname, './certs/key.pem')),
-        cert: readFileSync(resolve(__dirname, './certs/cert.pem')),
-      },
-    },
+    server: devServerOptions,
     root: clientRoot,
   });
 
-const clientBuildConfig = ({ clientEntrypointRoot, template }) =>
+const clientBuildConfig = ({
+  clientEntrypointRoot,
+  template,
+}: {
+  clientEntrypointRoot: string;
+  template: string;
+}) =>
   defineConfig({
     plugins: [react(), viteSingleFile({ useRecommendedBuildConfig: true })],
     root: resolve(__dirname, clientRoot, clientEntrypointRoot),
@@ -67,7 +96,6 @@ const clientBuildConfig = ({ clientEntrypointRoot, template }) =>
       emptyOutDir: true,
       minify: true,
       rollupOptions: {
-        // add externals for react and react-dom
         external: [
           'react',
           'react-dom',
@@ -99,22 +127,37 @@ const clientBuildConfig = ({ clientEntrypointRoot, template }) =>
     },
   });
 
-const serverConfig = ({
-  mode,
-  emptyOutDir = true,
-}: {
-  mode: string;
-  emptyOutDir?: boolean;
-}) => {
-  const targets = [{ src: 'appsscript.json', dest: './' }];
+const serverBuildConfig: BuildOptions = {
+  emptyOutDir: true,
+  minify: false, // needed to work with footer
+  lib: {
+    entry: resolve(__dirname, serverEntry),
+    fileName: 'code',
+    name: 'globalThis',
+    formats: ['iife'],
+  },
+  rollupOptions: {
+    output: {
+      entryFileNames: 'code.js',
+      extend: true,
+      footer: (chunk) =>
+        chunk.exports
+          .map((exportedFunction) => `function ${exportedFunction}() {};`)
+          .join('\n'),
+    },
+  },
+};
+
+const buildConfig = ({ mode }: { mode: string }) => {
+  const targets = [{ src: copyAppscriptEntry, dest: './' }];
 
   if (mode === 'development') {
     targets.push(
       ...clientEntrypoints.map((entrypoint) => ({
-        src: 'dev/dev-server-wrapper.html',
+        src: devServerWrapper,
         dest: './',
         rename: entrypoint.filename + '.html',
-        transform: (contents, filename) =>
+        transform: (contents: string) =>
           contents
             .toString()
             .replace('__PORT__', String(PORT))
@@ -127,6 +170,13 @@ const serverConfig = ({
       viteStaticCopy({
         targets,
       }),
+      /**
+       * This builds the client react app bundles for production, and writes them to disk.
+       * Because multiple client entrypoints (dialogs) are built, we need to loop through
+       * each entrypoint and build the client bundle for each. Vite doesn't have great tooling for
+       * building multiple single-page apps in one project, so we have to do this manually with a
+       * post-build closeBundle hook (https://rollupjs.org/guide/en/#closebundle).
+       */
       mode === 'production' && {
         name: 'build-client-production-bundles',
         closeBundle: async () => {
@@ -139,60 +189,28 @@ const serverConfig = ({
                 template: clientEntrypoint.template,
               })
             );
-
             await writeFile(
               resolve(__dirname, outDir, clientEntrypoint.filename + '.html'),
               // @ts-expect-error - output is an array of RollupOutput
               buildOutput.output[0].source
             );
           }
-
-          console.log('Finished building client bundles!'); // run during closeBundle hook. https://rollupjs.org/guide/en/#closebundle
+          console.log('Finished building client bundles!');
         },
       },
     ].filter(Boolean),
-    build: {
-      emptyOutDir,
-      minify: false, // needed to work with footer
-      lib: {
-        entry: resolve(__dirname, 'src/server/index.ts'),
-        fileName: 'code',
-        name: 'globalThis',
-        formats: ['iife'],
-      },
-      rollupOptions: {
-        output: {
-          entryFileNames: 'code.js',
-          extend: true,
-          footer: (chunk) =>
-            chunk.exports
-              .map((exportedFunction) => `function ${exportedFunction}() {};`)
-              .join('\n'),
-        },
-      },
-    },
+    build: serverBuildConfig,
   });
 };
 
 // https://vitejs.dev/config/
-export default async ({ command, mode, isSsrBuild, isPreview }) => {
+export default async ({ command, mode }: { command: string; mode: string }) => {
   if (command === 'serve') {
     // for 'serve' mode, we only want to serve the client bundle locally
     return clientServeConfig();
   }
   if (command === 'build') {
     // for 'build' mode, we have two paths: build assets for local development, and build for production
-
-    if (mode === 'development') {
-      // when building for development, we want to build the "dev" wrapper for each client entrypoint
-      console.log('buildling client dev config');
-      return serverConfig({ mode, emptyOutDir: true });
-    }
-    if (mode === 'production') {
-      // When building for production, we want to build the server code and client code.
-      // For the client code, we want to build separate single-page html files for each entrypoint.
-      return serverConfig({ mode, emptyOutDir: true });
-    }
-    return serverConfig({ mode });
+    return buildConfig({ mode });
   }
 };
